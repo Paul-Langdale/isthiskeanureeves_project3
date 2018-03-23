b818cf41f6ed5f503fb5a4dc8d2bc834c569fc8c
HYUNJUN KIM <2335980k@student.gla.ac.uk>
2018-03-23 13:45:29 +0000

    add broken users to populate.py

--- isthiskeanureeves/models.py
+++ isthiskeanureeves/models.py
@@ -42,8 +42,14 @@ class Page(models.Model):
 class UserProfile(models.Model):
    
     user = models.OneToOneField(User)
+    username_slug = models.SlugField(unique=True)
     #website = models.URLField(blank=True)
     picture = models.ImageField(upload_to='profile_images', blank=True)
+    
+    def save(self, *args, **kwargs):
+        self.username_slug = slugify(self.user.username)
+        super(UserProfile, self).save(*args, **kwargs)
+
    
     def __str__(self):
         return self.user.username
--- populate_isthiskeanu.py
+++ populate_isthiskeanu.py
@@ -4,7 +4,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'isthiskeanureeves_project.setti
 import django
 django.setup()
 from isthiskeanureeves.models import Category, Page, UserProfile, User
-
+from django.template.defaultfilters import slugify
 def populate():
     keanothim = [{"user": "2250052l","title": "Not Keanu"}]
     keanew = [{"user": "2250052l","title": "Young Keanu"}]
@@ -19,6 +19,52 @@ def populate():
 
 
 
+
+    users = [
+            {"username": "2335980k",
+             "email": "2335980k@student.gla.ac.uk",
+             "password": "asd5849"
+             },
+             {"username": "HATEkeanu",
+             "email": " ",
+             "password": "a1234567"
+             },
+             {"username": "Yoshi",
+             "email": " ",
+             "password": "a1234567"
+             },
+             {"username": "fakekeanu",
+             "email": " ",
+             "password": "a1234567"
+             },
+             {"username": "hohohoho",
+             "email": " ",
+             "password": "a1234567"
+             },
+             {"username": "hoya",
+             "email": "kkkk@gmail.com ",
+             "password": "a1234567"
+             },
+             {"username": "keanuholic",
+             "email": " ",
+             "password": "a1234567"
+             },
+             {"username": "testprofile",
+             "email": " ",
+             "password": "a1234567"
+             }]
+
+    #for i in range(len(users)):
+     #   new_user = users[i]
+      #  u = add_user(new_user['username'],
+       #              new_user['email'],
+        #             new_user['password'])
+
+
+
+
+
+
       
 #def add_category(name, image):
 def add_category(name):
@@ -27,6 +73,16 @@ def add_category(name):
     #c.rating = rating //OUT
     c.save()
     return c
+
+#def add_user(username, email, password): // populate problem. ############################:
+def add_user(username, email, password):
+    user = User.objects.get_or_create(username=username, email=email)[0]
+    user.set_password(password)
+    user.save()
+    slug_username = slugify(username)
+    u = UserProfile.objects.get_or_create(username_slug=slug_username, user_id=user.id)[0]
+    u.save()
+    return u
 
 # Starts execution here
 if __name__ == '__main__':
